2c9a73732db6b976df1d9c84719db65134d744cc
qLau <38216868+qLau@users.noreply.github.com>
2018-04-28 21:33:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,5 +4,9 @@ const bot = new Discord.Client();
 bot.on('guildMemberAdd', function(member) {
     member.guild.channels.find("name", "new-faggs").sendMessage(`Bine ai venit pe server, ${member} tocmai ai devenit un new fag!\n:black_medium_small_square:  Nu avem reguli, dar totuși sperăm să te comporți cât de cât omenește.`);
   });
+bot.on("ready", async () => {
+    console.log(`Legion Guard este online`);
+    bot.user.setGame(`discord.io/legione`)
+});
   
 bot.login(process.env.BOT_TOKEN);

4e98a705a655a0eb8ba6f433a773dfd86bf6c767
qLau <38216868+qLau@users.noreply.github.com>
2018-04-28 21:05:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('guildMemberAdd', function(member) {
-    member.guild.channels.find("name", "welcome").sendMessage(`Bine ai venit pe server, ${member} tocmai ai devenit un new fag!\n:black_medium_small_square:  Nu avem reguli, dar totuși sperăm să te comporți cât de cât omenește.`)
-    let role = member.guild.roles.find("name", "Membru");
-    member.addRole(role)
+    member.guild.channels.find("name", "welcome").sendMessage(`Bine ai venit pe server, ${member} tocmai ai devenit un new fag!\n:black_medium_small_square:  Nu avem reguli, dar totuși sperăm să te comporți cât de cât omenește.`);
   });
   
   bot.login(process.env.BOT_TOKEN);

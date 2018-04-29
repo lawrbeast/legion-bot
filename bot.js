7abad8ffbb38d07b1c626d7d62052dc408ae35a5
qLau <38216868+qLau@users.noreply.github.com>
2018-04-29 22:39:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,28 +47,23 @@ bot.on("message", async message => {
      message.channel.send({embed});
     }
   if(cmd === `${prefix}serverinfo`){
-    let online = message.guild.members.filter(member => member.user.presence.status !== 'offline');
-let day = message.guild.createdAt.getDate()
-let month = 1 + message.guild.createdAt.getMonth()
-let year = message.guild.createdAt.getFullYear()
- let sicon = message.guild.iconURL;
- let serverembed = new Discord.RichEmbed()
- .setAuthor(message.guild.name, sicon)
- .setFooter(`Server Created • ${day}.${month}.${year}`)
- .setColor("#7289DA")
- .setThumbnail(sicon)
- .addField("ID", message.guild.id, true)
- .addField("Name", message.guild.name, true)
- .addField("Owner", message.guild.owner.user.tag, true)
- .addField("Region", message.guild.region, true)
- .addField("Channels", message.guild.channels.size, true)
- .addField("Members", message.guild.memberCount, true)
- .addField("Humans", message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size, true)
- .addField("Bots", message.guild.members.filter(m => m.user.bot).size, true)
- .addField("Online", online.size, true)
- .addField("Roles", message.guild.roles.size, true);
- message.channel.send(serverembed);
-  }
+
+        let sicon = message.guild.iconURL;
+        let serverembed = new Discord.RichEmbed()
+        .setColor("#0d1d38")
+        .setThumbnail(sicon)
+        .addField("ID:", message.guild.id)
+        .addField("Membrii", `[${message.guild.memberCount}]`, true)
+        .addField("Roluri:", `[${message.guild.roles.size}]`, true)
+        .addField("Emojis:", `[${message.guild.emojis.size}]`)
+        .addField("Canale:", `[${message.guild.channels.size}]`, true)
+        .addField("Regiune:", message.guild.region, true)
+        .addField("Server Owner:", `👑${message.guild.owner}👑`)
+        .setFooter(`${message.guild.name}`, message.guild.iconURL)
+        .setTitle(`♦${message.guild.name}♦`, message.guild.iconURL);
+
+        return message.channel.send(serverembed);
+    }
   });
   
 bot.login(process.env.BOT_TOKEN);

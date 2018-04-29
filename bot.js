c6ab51c7746afa6aea2f78464931896dde30b4f4
qLau <38216868+qLau@users.noreply.github.com>
2018-04-29 22:19:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,5 +8,43 @@ bot.on("ready", async () => {
     console.log(`Legion Guard este online`);
     bot.user.setPresence({ stream: { name: 'discord.io/legione', type: 1 } });
 });
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return
+
+    const prefix = "$";
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let sender = message.author;
+    let args = messageArray.slice(1);
+   
+  if(cmd === `${prefix}userinfo`){
+	let user;
+	// If the user mentions someone, display their stats. If they just run userinfo without mentions, it will show their own stats.
+    if (message.mentions.users.first()) {
+      user = message.mentions.users.first();
+    } else {
+        user = message.author;
+    }
+	// Define the member of a guild.
+    const member = message.guild.member(user);
+
+	//Discord rich embed
+    const embed = new Discord.RichEmbed()
+		.setColor('RANDOM')
+		.setThumbnail(user.avatarURL)
+		.setTitle(`${user.username}#${user.discriminator}`)
+		.addField("ID:", `${user.id}`, true)
+		.addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+		.addField("Creat pe", `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+		.addField("A intrat pe server:", `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+		.addField("Bot:", `${user.bot}`, true)
+		.addField("Status:", `${user.presence.status}`, true)
+		.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+		.addField("Roles:", member.roles.map(roles => `${roles.name}`).join(', '), true)
+		.setFooter(`Cerut de ${message.author.username}#${message.author.discriminator}`)
+     message.channel.send({embed});
+    }
+  }
   
 bot.login(process.env.BOT_TOKEN);

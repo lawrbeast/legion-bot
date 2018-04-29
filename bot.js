6cf0eabecfb051e891814f00203d04c3f9225f4e
qLau <38216868+qLau@users.noreply.github.com>
2018-04-29 23:00:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,17 +48,12 @@ bot.on("message", message => {
     }
 	//AVATAR
   if(cmd === `${prefix}avatar`){
-	    // Define user, if nobody is mentioned it will store author
     let user = message.mentions.users.first() || message.author;
-    
-    // Form Embed
-    const embed2 = new Discord.RichEmbed()
+    const server = new Discord.RichEmbed()
         .setColor(0xffffff) // This will set the embed sidebar color
         .setTitle(user.username) // This will set the embed title
-        .setImage(user.avatarURL); // This will set the embed image
-        
-    // Send Message
-    message.channel.send({ embed2} );
+        .setImage(user.avatarURL); // This will set the embed image     
+    message.channel.send({ server });
         return;
     }
   });

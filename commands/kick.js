02ab8cd2b6523bc7d7d34fa9ab974aa9acf116e5
qLau <38216868+qLau@users.noreply.github.com>
2018-05-06 22:14:54 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,11 +6,12 @@ module.exports.run = async (bot, message, args) => {
         let kickuEmbed = new Discord.RichEmbed()
         .setColor(`#0xffffff`)
         .setDescription("Pentru a da kick\nFoloseste:\n$kick <user>")
+        if(
 
-        let cfu = new Discord.RichEmbed()
+        const cfu = new Discord.RichEmbed()
         .setColor("#f90000")
         .setDescription("Nu gasesc acel user.")
-        if(!kUser) return message.channel.send(cfu).then(msg => {msg.delete(5000)});
+        if(!kUser) return message.channel.send({cfu}).then(msg => {msg.delete(5000)});
         let kReason = args.join(" ").slice(22);
         if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nu ai permisiunile necesare.");
         if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("Nu poti face asta unui administrator.");

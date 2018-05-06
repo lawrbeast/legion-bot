44d2bf1142c4734369ca76d419c89542c77d828a
qLau <38216868+qLau@users.noreply.github.com>
2018-05-06 21:24:20 +0300

    Create gif.js

---
+++ commands/gif.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const gifSearch = require("gif-search");
+
+exports.run = (bot, message, args) => {
+  if (message.channel.type == "dm") return;
+
+    gifSearch.random(args[0]).then(
+        gifUrl => {
+        const embed = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .setImage(gifUrl)
+        message.channel.send({embed});
+    });
+
+}

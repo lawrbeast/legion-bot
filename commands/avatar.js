532d96eb19716e7da2b0f63d2385b80c23fb66f2
qLau <38216868+qLau@users.noreply.github.com>
2018-05-06 21:58:30 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 
-exports.run = (client, message, args, tools) => {
+exports.run = (bot, message, args, tools) => {
     
     // Define user, if nobody is mentioned it will store author
     let user = message.mentions.users.first() || message.author;
     
     // Form Embed
-    const embed = new Discord.MessageEmbed()
+    const embed = new Discord.RichEmbed()
         .setColor(0xffffff) // This will set the embed sidebar color
         .setTitle(user.username) // This will set the embed title
         .setImage(user.avatarURL({size: 2048})) // This will set the embed image
         
     // Send Message
-    message.channel.send(embed)
+    message.channel.send({embed})
     
 }
 

a7e7f27e0ddf5246bf5156de470ff638a685d827
qLau <38216868+qLau@users.noreply.github.com>
2018-05-07 21:17:54 +0300

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const meme = require('memejs');
 
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+exports.run = async (bot, message, args, level) => { // eslint-disable-line no-unused-vars
   meme(function(data) {
   const embed = new Discord.RichEmbed()
   .setTitle(data.title[0])

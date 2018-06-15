b6c5538247b534321bc860f605e4e3566c0166e1
qLau <38216868+qLau@users.noreply.github.com>
2018-06-15 22:32:52 +0300

    Create fmk.js

---
+++ commands/fmk.js
@@ -0,0 +1,23 @@
+// Fuck, Marry, Kill - Command by DRYNO!
+// Make sure you call the discord.js package!
+const Discord = require('discord.js');
+
+// Make sure you use exports.run when you have a command handler!
+module.exports.run = (bot, message, args) => {
+
+    let replies = ['MARRY :ring:', 'KILL :bomb:', 'FUCK :ok_hand:'];
+    let result = Math.floor(Math.random() * replies.length);
+
+    let makifuembed = new Discord.RichEmbed()
+
+        .setDescription(`**${args[0]} has been choosed by <@${message.author.id}>**`)
+        .setColor('RANDOM')
+        .addField('You choosed:', replies[result])
+        .setFooter('Fuck, Marry, Kill!', bot.user.displayAvatarURL)
+        .setTimestamp();
+    
+    message.channel.send({embed: makifuembed});
+    
+    module.exports.help = {
+      name: "fmk"
+      }

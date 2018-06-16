addd60d48df2456d2908223e5285ebc3a8c1c2ec
qLau <38216868+qLau@users.noreply.github.com>
2018-06-16 20:07:27 +0300

    Update muye.js

--- commands/muye.js
+++ commands/muye.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 exports.run = (client, message, args, tools) => {
   let muyeembed = new Discord.RichEmbed()
   .setTitle('Sistem Muye')
-  .setDescription('${args[0]} a primit muYe de la ${message.author}')
+  .setDescription(`${args[0]} a primit muYe de la ${message.author}`)
   .setThumbnail('https://discordapp.com/assets/209381ec0f39a61c1904269ed41c62eb.svg')
   .setFooter('Sistem facut de cel mai developer qLau aka Seful Banilor aka aka.')
 message.channel.send({embed:muyeembed})

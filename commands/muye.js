29e7efae7e5dc9cf0dd1145cc02ec4d38be359cc
qLau <38216868+qLau@users.noreply.github.com>
2018-06-16 20:09:45 +0300

    Update muye.js

--- commands/muye.js
+++ commands/muye.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args, tools) => {
   let muyeembed = new Discord.RichEmbed()
   .setTitle('Sistem Muye :middle_finger:')
   .setDescription(`${args[0]} a primit muYe de la ${message.author}`)
-  .setThumbnail('http://www.memegen.com/meme/bp7m3n')
+  .setThumbnail('http://moftul-roman.blogspot.com/2012/09/muie-ti-am-spus-traian-basescu.html')
   .setFooter('Sistem facut de cel mai developer qLau aka Seful Banilor aka aka.')
 message.channel.send({embed:muyeembed})
 }

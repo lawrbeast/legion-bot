df5d7b194570aed757184ca7794e4138a2d61e67
qLau <38216868+qLau@users.noreply.github.com>
2018-06-16 20:12:04 +0300

    Update muye.js

--- commands/muye.js
+++ commands/muye.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args, tools) => {
   let muyeembed = new Discord.RichEmbed()
   .setTitle('Sistem Muye :middle_finger:')
   .setDescription(`${args[0]} a primit muYe de la ${message.author}`)
-  .setThumbnail('http://moftul-roman.blogspot.com/2012/09/muie-ti-am-spus-traian-basescu.html')
+  .setThumbnail('http://www.emoji.co.uk/?s=middle%20finger')
   .setFooter('Sistem facut de cel mai developer qLau aka Seful Banilor aka aka.')
 message.channel.send({embed:muyeembed})
 }

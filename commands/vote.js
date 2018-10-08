73d94db2301324a6ce856c9784d15266a62be870
qLau <38216868+qLau@users.noreply.github.com>
2018-10-08 21:02:47 +0300

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -6,7 +6,7 @@ let voteembed = new Discord.RichEmbed()
 .setColor("RANDOM")
 .setTitle("Voteaza!")
 .setDescription(`🔊\n${args.join(" ")}`)
-.setFooter("Primal | qLau ©}");
+.setFooter("Primal | qLau ©");
 
 const msg = await message.channel.send({embed:voteembed})
 await msg.react("✅");

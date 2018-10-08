a6bb739877f2ced8a575d5fcdaf2a5152a922f50
qLau <38216868+qLau@users.noreply.github.com>
2018-10-08 20:53:50 +0300

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, prefix) => {
+
+let voteembed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setDescription(`**Voteaza!**\n\n${args.join(" ")}\n\nVot creeat de ${message.author}`)
+.setFooter("Primal | qLau ©");
+
+const msg = await message.channel.send(voteembed)
+await msg.react("✅");
+await msg.react("❎");
+
+};
+module.exports.help = {
+    name: "vote"
+};

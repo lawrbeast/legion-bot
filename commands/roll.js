021d2344c86b829949ca35fd28f910f1577880fe
qLau <38216868+qLau@users.noreply.github.com>
2018-10-08 20:40:31 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,17 +1,20 @@
-const Discord = require("discord.js");
-exports.run = async (bot, message, args, level) => {
-
-  let replies = ["1", "2", "3", "4", "5", "6", "Nimic"];
-
-  let result = Math.floor((Math.random() * replies.length));
-
-  const embed = new Discord.RichEmbed()
-  .setAuthor(message.author.tag)
-  .setColor("#000000")
-  .setDescription(`Arunca un zar si cade numarul: **${replies[result]}**`)
-
-  message.channel.send({embed});
+const Discord = module.require("discord.js");
+const randomnum = require("unique-random");
+const rand = randomnum(1, 6);
+const rand1 = randomnum(1, 6);
+module.exports.run = async (bot, message, args) => {
+if(!args[0]){
+    await message.channel.send("Arunc zarul...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
+    await message.channel.send(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`);
 }
-exports.help = {
-  name: "roll"
+if(args[0] === "duel"){
+    let userduel = args[1];
+    if(!userduel) return message.channel.send("**Exemplu: d!roll duel @user**");
+    await message.channel.send("Arunc zarurile...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
+    await message.channel.send(`:game_die: Zarurile au cazut pe fetele cu numarul:\nJucator 1: ${message.author}: ${rand()}\nJucator 2: ${userduel}: ${rand1()}`);
+ }
+}
+
+module.exports.help = {
+    name: "roll"
 }

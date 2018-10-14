b64a3155cc1fd70955bba2ce6eaeaad472d898a3
qLau <38216868+qLau@users.noreply.github.com>
2018-10-14 22:20:11 +0300

    Create dick.js

---
+++ commands/dick.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+
+  let dick = [1, 27, 12, 7, 10, 69, 40, 23, 5, 68, 54, 43]
+  let dickres = Math.floor(Math.random() * dick.length);
+
+  let dickembed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle("🔞 Verifica-ti marimea dick-ului!")
+  .addField("Tu ai un dick de", `${dick[dickres]} cm`)
+  .setFooter("Dreamer! | qLau ©")
+
+  message.channel.send(dickembed);
+
+}
+
+module.exports.help = {
+  name: "dick"
+}

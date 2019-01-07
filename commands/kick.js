506983d1ea2ff29f2d57123b72e9f07d0bf0275f
qLau <38216868+qLau@users.noreply.github.com>
2019-01-08 00:53:50 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,12 +3,25 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
         let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-        if(!kUser) return message.channel.send("**Nu gasesc acel user!***").then(msg => {msg.delete(5000)});
+        let lopEmbed = new Discord.RichEmbed()
+        .setColor('#dd0d02')
+        .setDescription(':x: Nu poti face asta deoarece nu ai permisiunile necesare. ')
+        let indEmbed = new Discord.RichEmbed()
+        .setColor('#dd0d02')
+        .setDescription(':x: Nu poti face asta unui administrator.')
+         let unfEmbed = new Discord.RichEmbed()
+        .setColor('#dd0d02')
+        .setDescription(':x: Nu poti face asta unui administrator.')
+        
+        if(!kUser) return message.channel.send({embed:unfEmbed}).then(msg => {msg.delete(5000)});
         let kReason = args.join(" ").slice(22);
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nu ai permisiunile necesare.");
-        if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("Nu poti face asta unui administrator.");
-
-        message.guild.member(kUser).kick(kReason).then(msg => {message.channel.send(`**${kUser} a primit kick :white_check_mark:!**`)});
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send({embed:lopEmbed});
+        if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send({embed:indEmbed});
+        
+        let kickedEmbed = new Discord.RichEmbed()
+        .setColor('#0ef24b')
+        .setDescription('✅ ${kuser} a primit kick!')
+        message.guild.member(kUser).kick(kReason)
 }
 
 module.exports.help = {

f565c0b2a91352bdd334ef0fdcd5c19940e491f3
qLau <38216868+qLau@users.noreply.github.com>
2019-01-11 23:36:15 +0200

    Update playstatus.js

--- commands/playstatus.js
+++ commands/playstatus.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const snekfetch = require('snekfetch')
 
 module.exports.run = async (bot, message, args) => {
-    const url = `http://mcapi.us/server/image?ip=85.10.192.217&theme=dark`;
+    const url = `http://mcapi.us/server/image?ip=85.10.192.217&theme=dark&title=PLAY.MEDIEVALMC.RO`;
     snekfetch.get(url)
         .then(r => message.channel.send("", { files: [{ attachment: r.body }] }));
 }

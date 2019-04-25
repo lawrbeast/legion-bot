63392c41fc2cb61f910beb8e4bf1b101270a093d
qLau <38216868+qLau@users.noreply.github.com>
2019-04-26 02:37:26 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -5,30 +5,41 @@ exports.run = (client, message, args, ops) => { //Collecting info about command
 
   let fetched = ops.active.get(message.guild.id);
 
-  if (!fetched) return message.channel.send("Nu este nicio melodie în lista de așteptare!");
-  if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Nu ești în același canal!");
+  if (!fetched) return message.channel.send("Сейчас ничего не играет! Используй `play <url>|<song>` чтобы поставить композицию в очередь.");
+  if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Ты должен быть в одном канале с ботом!");
 
   var userCount = message.member.voiceChannel.members.size;
   var required = Math.ceil(userCount / 2);
 
-  if (!fetched.queue.voteSkips) {
+  if (!fetched.queue[0].voteSkips) {
     fetched.queue[0].voteSkips = [];
   }
 
   if (fetched.queue[0].voteSkips.includes(message.member.id)) {
     return message.channel.send({
       embed: {
-        "title": "Ai votat deja pentru skip!",
-        "description": "Ce a rămas: " + fetched.queue[0].voteSkips.length / required,
-        "color": 0x76e8d8
+        "title": "You are already voted for skip!",
+        "description": "Left: " + fetched.queue[0].voteSkips.length / required,
+        "color": 0xff2222
       }
     });
   }
 
-      fetched.queue[0].voteSkips.push(message.member.id);
-      ops.active.set(message.guild.id, fetched);
-  
-    if (!fetched.queue[0].length == 0) {
+  fetched.queue[0].voteSkips.push(message.member.id);
+  ops.active.set(message.guild.id, fetched);
+
+  if (fetched.queue[0].voteSkips.length >= required) {
+    message.channel.send({
+      embed: {
+        "title": "Song skipped!",
+        "color": 0x22ff22
+      }
+    }).then(msg => {
+      if (conf[message.guild.id].delete == 'true') {
+        msg.delete(conf[message.guild.id].deleteTime);
+      }
+    });
+    if (!fetched.queue.length == 0) {
       return fetched.dispatcher.emit('finish');
     } else {
       return fetched.dispatcher.end();
@@ -38,10 +49,14 @@ exports.run = (client, message, args, ops) => { //Collecting info about command
 
   message.channel.send({
     embed: {
-      "title": "A fost votat!",
-      "description": "Ce a rămas: " + Math.ceil(fetched.queue[0].voteSkips.length / required),
-      "color": 0x76e8d8
+      "title": "Voted!",
+      "description": "Left: " + Math.ceil(fetched.queue[0].voteSkips.length / required),
+      "color": 0x22ff22
     }
-  })
+  }).then(msg => {
+    if (conf[message.guild.id].delete == 'true') {
+      msg.delete(conf[message.guild.id].deleteTime);
+    }
+  });
 
 }

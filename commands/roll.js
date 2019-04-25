9d66c7e87d3c8423d2ea566a53a2c225d5e7d5ce
qLau <38216868+qLau@users.noreply.github.com>
2019-04-25 21:27:58 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -3,16 +3,9 @@ const randomnum = require("unique-random");
 const rand = randomnum(1, 6);
 const rand1 = randomnum(1, 6);
 module.exports.run = async (bot, message, args) => {
-if(!args[0]){
-    await message.channel.send("Arunc zarul...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
-    await message.channel.send(`:game_die: Zarul a cazut pe fata cu numarul: ${rand()}`);
-}
-if(args[0] === "duel"){
-    let userduel = args[1];
-    if(!userduel) return message.channel.send("**Exemplu: d!roll duel @user**");
-    await message.channel.send("Arunc zarurile...:game_die: :game_die: ").then(msg => {msg.delete(2500)});
-    await message.channel.send(`:game_die: Zarurile au cazut pe fetele cu numarul:\nJucator 1: ${message.author}: ${rand()}\nJucator 2: ${userduel}: ${rand1()}`);
- }
+
+    message.channel.send('reload merge boss')
+    
 }
 
 module.exports.help = {

8395726ac39c1579c53b7fa236398000ec8646c0
qLau <38216868+qLau@users.noreply.github.com>
2019-04-26 01:54:48 +0300

    Create skip.js

---
+++ commands/skip.js
@@ -0,0 +1,62 @@
+var fs = require('fs'); //FileSystem
+let conf = JSON.parse(fs.readFileSync("./config.json", "utf8")); //Config file
+
+exports.run = (client, message, args, ops) => { //Collecting info about command
+
+  let fetched = ops.active.get(message.guild.id);
+
+  if (!fetched) return message.channel.send("Nu este nicio melodie în lista de așteptare!");
+  if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Nu ești în același canal!");
+
+  var userCount = message.member.voiceChannel.members.size;
+  var required = Math.ceil(userCount / 2);
+
+  if (!fetched.queue[0].voteSkips) {
+    fetched.queue[0].voteSkips = [];
+  }
+
+  if (fetched.queue[0].voteSkips.includes(message.member.id)) {
+    return message.channel.send({
+      embed: {
+        "title": "Ai votat deja pentru skip!",
+        "description": "Ce a rămas: " + fetched.queue[0].voteSkips.length / required,
+        "color": 0xff2222
+      }
+    });
+  }
+
+  fetched.queue[0].voteSkips.push(message.member.id);
+  ops.active.set(message.guild.id, fetched);
+
+  if (fetched.queue[0].voteSkips.length >= required) {
+    message.channel.send({
+      embed: {
+        "title": "Melodia a fost sarită!",
+        "color": 0x22ff22
+      }
+    }).then(msg => {
+      if (conf[message.guild.id].delete == 'true') {
+        msg.delete(conf[message.guild.id].deleteTime);
+      }
+    });
+    if (!fetched.queue.length == 0) {
+      return fetched.dispatcher.emit('finish');
+    } else {
+      return fetched.dispatcher.end();
+    }
+    ops.active.set(message.guild.id, fetched);
+  }
+
+  message.channel.send({
+    embed: {
+      "title": "A fost votat!",
+      "description": "Ce a rămas: " + Math.ceil(fetched.queue[0].voteSkips.length / required),
+      "color": 0x22ff22
+    }
+  }).then(msg => {
+    if (conf[message.guild.id].delete == 'true') {
+      msg.delete(conf[message.guild.id].deleteTime);
+    }
+  });
+
+}

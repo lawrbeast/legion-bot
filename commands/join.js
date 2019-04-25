cd7e7ae89d452a0668453874bdfcf9846b45f773
qLau <38216868+qLau@users.noreply.github.com>
2019-04-25 21:11:03 +0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,8 +1,9 @@
 
 const Discord = require("discord.js");
 var fs = require('fs'); //FileSystem
+let conf = JSON.parse(fs.readFileSync("./config.json", "utf8")); //Config file
 
-exports.run = (bot, message, args) => {
+exports.run = (bot, message, args, ops) => {
   
   if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send({
     embed: {
@@ -10,7 +11,12 @@ exports.run = (bot, message, args) => {
       "color": 0xff2222,
       "title": "Error"
     }
-  })  
+  }).then(msg => {
+    if (conf[message.guild.id].delete == 'true') {
+      msg.delete(conf[message.guild.id].deleteTime);
+    }
+  });
+  
   const voiceChannel = message.member.voiceChannel;
   if (message.guild.me.voiceChannel) {
     return message.channel.send({
@@ -18,14 +24,23 @@ exports.run = (bot, message, args) => {
         "title": "Sorry, bot is already in voice channel!",
         "color": 0xff2222
       }
-    })
+    }).then(msg => {
+      if (conf[message.guild.id].delete == 'true') {
+        msg.delete(conf[message.guild.id].deleteTime);
+      }
+    });
+  }
   if (!voiceChannel) {
     return message.channel.send({
       embed: {
         "title": "You need to be in the same channel with me!",
         "color": 0xff2222
       }
-    })
+    }).then(msg => {
+      if (conf[message.guild.id].delete == 'true') {
+        msg.delete(conf[message.guild.id].deleteTime);
+      }
+    });
   }
   voiceChannel.join();
   message.channel.send({
@@ -34,4 +49,3 @@ exports.run = (bot, message, args) => {
       "color": 0x22ff22
     }
   })
-  }

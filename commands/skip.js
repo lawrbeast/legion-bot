e0ac901e6fa208cb2fd54d5b0f4de180a81b17c0
qLau <38216868+qLau@users.noreply.github.com>
2019-04-26 03:17:03 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -4,12 +4,23 @@ let conf = JSON.parse(fs.readFileSync("./config.json", "utf8")); //Config file
 exports.run = (client, message, args, ops) => { //Collecting info about command
 
   let fetched = ops.active.get(message.guild.id);
-
-  if (!fetched) return message.channel.send("Сейчас ничего не играет! Используй `play <url>|<song>` чтобы поставить композицию в очередь.");
-  if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Ты должен быть в одном канале с ботом!");
   
-  ops.active.set(message.guild.id, fetched);
-
-  return fetched.dispatcher.end();
-
+  if(!fetched) return message.channel.send(`There is currently nothing playing.`)
+  
+  if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send(`We aren\'t in the same channel!`)
+  
+  let userCount = message.member.voiceChannel.members.size;
+  let required = Math.ceil(userCount/2);
+  if(!fetched.queue[0].voteSkips) fetched.queue[0].voteSkips = [];
+  
+  if(fetched.queue[0].voteSkips.includes(message.member.id)) return message.channel.send(`Sorr you already voted. ${fetched.queue[0].voteSkips.length}/${required}`)
+  
+  fetched.queue[0].voteSkips.push(message.member.id)
+  ops.active.set(message.guild.id, fetched)
+  
+  if(fetched.queue[0].voteSkips.length >= required){
+    message.channel.send(`Song skipped!`)
+    return fetched.dispatcher.end();
+  }
+  message.channel.send(`Vote added!`)
 }

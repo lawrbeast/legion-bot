8a33b3fd8a8df4b718fb08556add49f602e5f33a
qLau <38216868+qLau@users.noreply.github.com>
2019-04-26 03:03:38 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -7,43 +7,9 @@ exports.run = (client, message, args, ops) => { //Collecting info about command
 
   if (!fetched) return message.channel.send("Сейчас ничего не играет! Используй `play <url>|<song>` чтобы поставить композицию в очередь.");
   if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Ты должен быть в одном канале с ботом!");
-
-  var userCount = message.member.voiceChannel.members.size;
-  var required = Math.ceil(userCount / 2);
-
-  if (!fetched.queue[0].voteSkips) {
-    fetched.queue[0].voteSkips = [];
-  }
-
-  if (fetched.queue[0].voteSkips.includes(message.member.id)) {
-    return message.channel.send({
-      embed: {
-        "title": "You are already voted for skip!",
-        "description": "Left: " + fetched.queue[0].voteSkips.length / required,
-        "color": 0xff2222
-      }
-    });
-  }
-
-  fetched.queue[0].voteSkips.push(message.member.id);
+  
   ops.active.set(message.guild.id, fetched);
 
-  if (fetched.queue[0].voteSkips.length >= required) {
-    message.channel.send({
-      embed: {
-        "title": "Song skipped!",
-        "color": 0x22ff22
-      }
-    })
-    return fetched.dispatcher.end();
-  }
-
-  message.channel.send({
-    embed: {
-      "title": "Voted!",
-      "description": "Left: " + Math.ceil(fetched.queue[0].voteSkips.length / required),
-      "color": 0x22ff22
-    }
-  })
+  return fetched.dispatcher.end();
 
 }

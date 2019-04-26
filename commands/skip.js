4de6f07a9afec82aeea236a0e8021259eb78ce7e
qLau <38216868+qLau@users.noreply.github.com>
2019-04-26 03:23:46 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -20,7 +20,7 @@ exports.run = (client, message, args, ops) => { //Collecting info about command
   
   if(fetched.queue[0].voteSkips.length >= required){
     message.channel.send(`Song skipped!`)
-    return fetched.dispatcher.end();
+    return fetched.dispatcher.emit('finish');
   }
   message.channel.send(`Vote added!`)
 }
